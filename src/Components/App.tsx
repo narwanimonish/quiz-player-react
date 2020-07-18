cface4827ba8689f5cb5cff93869e1014f16461c
Monish Narwani <narwanimonish@gmail.com>
2020-07-18 23:24:17 +0530

    Setup project, added bootstrap & SCSS support

---
+++ src/Components/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="bg-white rounded box-shadow">
+        <div className="m-4">
+          <h6 className="border-bottom p-3">Question</h6>
+          <p className="p-3">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam
+            doloribus minima repellat assumenda ipsum qui, praesentium eaque
+            reprehenderit laudantium nemo ut modi perspiciatis, quasi facere aut
+            voluptas deleniti sequi similique!
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
